ce158a9429260bb3e2555a43072b09f389ca8e9a
xurun2018 <xurun2018@gmail.com>
2018-08-10 19:15:44 +0800

    123

--- init_db_struct.sh
+++ init_db_struct.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
-python_upgrade_list='()'
-go_upgrade_list='(earth|taurus)'
-WORKSPACE=‘/home/xr/Galaxy’
+python_upgrade_list='(antlia|cassiopeia|circini|grus|)'
+go_upgrade_list='(aquarius|capricorn|aries|earth|hydra|leo|neptune|pisces|polaris)'
+#WORKSPACE='${HOME}/Galaxy'
+WORKSPACE="${HOME}/Desktop/xurun/Galaxy"
 
 
 function go_upgrade(){
     srv=$1
-    CONF=stage bazel run //${srv}/cmd/server:server -- -c ${WORKSPACE}/configset -u
+    CONF_LEVEL=stage bazel run //${srv}/cmd/server:server -- -c ${WORKSPACE}/configset -u
 }
-    
+
+function get_snapshot(){
+
+    LUPUS_PATH=‘${WORKSPACE}/configset/corp/lupus.json’
+    user=`cat lupus.json | jq -r .Database.User`
+    password=`cat lupus.json | jq -r .Database.Password`
+    database=`cat lupus.json | jq -r .Database.Database`
+    host=`cat lupus.json | jq -r .Database.Host`
+    port=`cat lupus.json | jq -r .Database.Port`
+    sql="select url from snapshot where type='SNAP_SHOT_CLOUD' order by created_time desc limit 1;"
+
+    return=`PGPASSWORD=${password} psql -h ${host} -U ${user} -d ${database} -p ${port} -c "${sql}"`
+
+    snap_shot=`echo ${res} | sed -e 's/-//g' | awk '{print $2}'`
+    export CIRCINI_SNAPSHOT_URL="${snap_shot}"
+
+}
+   
 function python_upgrade(){
     srv=$1
     cd ${WORKSPACE}/${srv}
-    ROOT_APP_PATH=‘${WORKSPACE}/${srv}’ python3 db_migration.py db init
-    ROOT_APP_PATH=‘${WORKSPACE}/${srv}’ python3 db_migration.py db upgrade
+    ROOT_APP_PATH='${WORKSPACE}/${srv}' python3 db_migration.py db init
+    ROOT_APP_PATH='${WORKSPACE}/${srv}' python3 db_migration.py db upgrade
 
 }
 
 
 
 
-
+cd ${WORKSPACE}
 
 for l in $@
 do
